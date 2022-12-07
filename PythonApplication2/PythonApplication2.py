a8c40cdc5dbb25180cf923b53d1e43e326c70b8e
opilane <opilane@HP-CZC6297BBM>
2022-12-07 16:41:59 +0200

    Add project files.

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,29 @@
+from math import * #oli vale kohas *
+import math
+print("Ruudu karakteristikud")
+a=float(input("Sisesta ruudu külje pikkus => ")) 
+S=a**2
+print("Ruudu pindala" ,round (S,1))
+P=4*a
+print("Ruudu ümbermõõt", P)
+di=a*math.sqrt(2)
+print("Ruudu diagonaal", round(di,2))
+print()
+print("Ristküliku karakteristikud")
+b=float(input("Sisesta ristküliku 1. külje pikkus => "))
+c=float(input("Sisesta ristküliku 2. külje pikkus => "))
+S=b*c
+print("Ristküliku pindala, S")
+P=2*(b+c)
+print("Ristküliku ümbermõõt",P)
+di=sqrt(b*2+c*2)
+print(f"Ristküliku diagonaal , {round(di,2)}")
+print()
+print("Ringi karakteristikud")
+r=float(input("Sisesta ringi raadiusi pikkus => " ))
+d=2*r
+print("Ringi läbimõõt", d)
+S=pi*r*2
+print("Ringi pindala", round(S,2)) #seal oli viga
+C=2*pi*r
+print("Ringjoone pikkus", round(C,2))
